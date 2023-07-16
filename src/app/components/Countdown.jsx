21867254abf698b1a5ae9bf3653fdd349de88ee6
Gino Pietrobon <gino.b.pietrobon@gmail.com>
2023-07-16 16:12:06 -0300

    add countdown

---
+++ src/app/components/Countdown.jsx
@@ -0,0 +1,37 @@
+'use client'
+import { useState, useEffect } from 'react';
+
+const Countdown = ({ targetDate }) => {
+  const [countdown, setCountdown] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = targetDate - now;
+
+      if (distance <= 0) {
+        clearInterval(interval);
+      } else {
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        setCountdown({ days, hours, minutes, seconds });
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [targetDate]);
+
+  return (
+    <h3 className='flex space-x-2 text-3xl'>     
+      <div>{countdown.days} días : </div>
+      <div>{countdown.hours} horas : </div>
+      <div>{countdown.minutes} min : </div>
+      <div>{countdown.seconds} seg </div>
+    </h3>
+  );
+};
+
+export default Countdown;
--- src/app/components/Section3.jsx
+++ src/app/components/Section3.jsx
@@ -1,16 +1,24 @@
-import {FiHeart} from "react-icons/fi"
+import { FiHeart } from "react-icons/fi";
+import Countdown from "./Countdown";
 
 export const Section3 = () => {
+  const targetDate = new Date("2024-03-03T00:00:00");
+
   return (
     <div className="flex flex-col text-white justify-center space-y-6 my-auto py-16 w-full items-center text-center bg-primary px-2 md:px-20 text-secundary">
+      <h3 className="text-3xl">#chinitaypablo</h3>
       <h4 className="text-5xl">¡Empezo la cuenta regresiva!</h4>
       <FiHeart className="text-5xl" />
-     
-      <h3 className="text-3xl">#chinitaypablo</h3>
-      <div className="italic">Te invitamos a festejar nuestro amor. ¡Nos encantaría que estuvieras allí!</div>
+
+      <Countdown targetDate={targetDate} />
+      <div className="italic text-xl py-8">
+        <p>Te invitamos a festejar nuestro amor.</p> <p>
+          ¡Nos encantaría que estuvieras
+          allí!
+        </p>
+      </div>
       <div className="text-3xl">3 de Marzo de 2024</div>
       <div>16:30 A 00:30</div>
-      
     </div>
   );
 };
