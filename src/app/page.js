e188ddb4b340baf39b98fef3e01e71405bd097ea
Gino Pietrobon <gino.b.pietrobon@gmail.com>
2023-07-20 10:21:02 -0300

    change page place

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,16 @@
-import Footer from "./components/Footer";
-import Section1 from "./components/Section1";
-import Section2 from "./components/Section2";
-import Section3 from "./components/Section3";
-import Section4 from "./components/Section4";
-import Section5 from "./components/Section5";
-import Section6 from "./components/Section6";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center"> 
-      <Section1 />
-      <Section2 />
-      <Section3 />
-      <Section4 />      
-      <Section5 />
-      <Section6 />
-      <Footer/>
+    <main className="flex min-h-screen flex-col items-center bg-primary text-secundary"> 
+     <div className="space-y-3 text-center my-auto">
+         <h1 className="text-7xl pb-6">Bienvenido a Invitaciones Web</h1>
+         <h3 className="text-3xl">Escribí o llamá al 2616708100 o enviá un mail a devzeppelin.ar@gmial.com para encargar tu propia invitación WEB</h3>
+         <h3 className="text-3xl">¡Nos encargamos de todo!</h3>
+         <h3 className="text-3xl">Código QR, Mapas interactivos, cuenta regresiva dinámica...</h3>
+         <h3 className="text-3xl">Vos elegí los colores y listo, rompela toda con tu tarjeta WEB</h3>
+         <h3 className="text-3xl">¡Gracias!</h3>
+     </div>
+      
     </main>
   );
 }
